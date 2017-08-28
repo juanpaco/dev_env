c029c6c6b6c60c993fbfafcb6312e830a64014c8
Ethan Garofolo <juanpaco@24potato.aml.cs.byu.edu>
2017-08-28 14:01:44 -0600

    Simplify many things and do symlinks instead of cp... it isn't 1995 anymore

--- git_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git config --global color.diff auto
-git config --global color.status auto
-git config --global color.branch auto
-
-cp .git-completion.bash ~/.git-completion.bash
-
--- install.sh
+++ install.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-cp .vimrc ~/.vimrc
-rm -Rf ~/.vim
-cp -R .vim ~/.vim
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+rm -Rf $HOME/.vim
+ln -s $(pwd)/.vim $HOME/.vim
 
-cp -R .tmux ~/.tmux
-cp .tmux.conf ~/.tmux.conf
-cp t.sh ~/t.sh
-cp tm ~/tm
+rm -Rf $HOME/.tmux
+ln -s $(pwd)/.tmux $HOME/.tmux
+ln -s $(pwd)/.tmux/layout_window $HOME/layout_window
+ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/t.sh $HOME/t.sh
+ln -s $(pwd)/tm $HOME/tm
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+######## Put some stuff in .bashrc
+echo "source $(pwd)/git-completion.bash" >> $HOME/.bashrc
+echo "source $(pwd)/git-prompt.sh" >> $HOME/.bashrc
+
+read -r -d '' VAR <<'EOF'
+PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+EOF
+
+echo $VAR >> $HOME/.bashrc
+echo "export VISUAL=vim" >> $HOME/.bashrc
+echo "export EDITOR=\"$VISUAL\"" >> $HOME/.bashrc
+######## END .bashrc
+
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
